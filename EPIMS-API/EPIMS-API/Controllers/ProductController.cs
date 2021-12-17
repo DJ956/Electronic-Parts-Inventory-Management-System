270013341eb647eb0c9611b0ea5290630175834a
DJ <dexter13576@gmail.com>
2021-12-18 01:11:06 +0900

    プロジェクト作成

---
+++ EPIMS-API/EPIMS-API/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EPIMS_API.Controllers
+{
+    /// <summary>
+    /// 製品コントローラー
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+
+
+    }
+}
